e87b17060c613da3420bcfd297a90c8ae91868d9
Jorge Pabon <jlp95@sunyorange.edu>
2025-09-19 12:49:37 -0400

    Lab 3 WIP

--- src/lab3/avgGrades.java
+++ src/lab3/avgGrades.java
@@ -1,6 +1,9 @@
 package lab3;
 import javax.swing.*;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Class that allows the user to enter grades and see
@@ -64,13 +67,19 @@ public class avgGrades {
 
     // Example Usage
     public static void main(String[] args) {
+        System.out.println("""
+                <-> Jorge Luis Pabon <->
+                <-> CSC 102 <->
+                <-> 9/19/2025 <->""");
+        // Array List to hold all the grades
         ArrayList<Double> gradeSheet = new ArrayList<>();
+        // Prompt
         String[] Options = {"Enter Grades Manually", "Search For File"};
         int Choice = JOptionPane.
                 showOptionDialog(null, "What Would You Like To Do?",
                         "Grade Machine 401", 0, 3,
                         null, Options, null);
-        // Manual Input
+        // 0 = Manual Input
         if (Choice == 0) {
             for (int i = 1; i <= 10; i++) {
                 gradeSheet.add(Double.parseDouble(JOptionPane.showInputDialog("Please Enter Grade #" + i)));
@@ -80,6 +89,18 @@ public class avgGrades {
             double avg = Rubric.calcAverage();
             JOptionPane.showMessageDialog(null, "Highest: "
                     + Rubric.getHighest() + "\nLowest: " + Rubric.getLowest() + "\nAverage: " + avg);
+        }
+        // 1 = Use A File
+        else if (Choice == 1) {
+            File TEST_SCORES_FILE = new File("testScores.txt");
+            File USERFILE = new File(JOptionPane.showInputDialog("What File Are You Using (default: testScores.txt)"));
+            // Try To Find The Users File
+            try (Scanner fileReader = new Scanner(USERFILE)) {
+                while (fileReader.hasNextLine()) {
+                    gradeSheet.add(fileReader.nextDouble());
+                }
+            }
+            catch (FileNotFoundException e) {}
         }
     }
 }
