8bd9c7f4bc0c237a331c937919d8f78d4d5ce740
Jorge Pabon <jorgepabon845@gmail.com>
2025-11-13 20:40:53 -0500

    Lab 5

---
+++ src/lab0/menuDisplay.java
@@ -0,0 +1,16 @@
+package lab0;
+
+public class menuDisplay implements genericMenu {
+    @Override
+    public int getInput() {
+        // Implementation for getting user input
+        return 0; // Placeholder return value
+    }
+
+    @Override
+    public void displayMenu() {
+        // Implementation for displaying the menu
+        System.out.println("Menu Displayed"); // Placeholder implementation
+    }
+
+}
